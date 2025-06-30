0c2634c36713fc14edfe4b93c6bf4b49777276f9
SYSTEMATI0N <133410295+SYSTEMATI0N@users.noreply.github.com>
2025-06-30 06:14:11 +0300

    Update run.sh

--- tetris_ha/run.sh
+++ tetris_ha/run.sh
@@ -1,7 +1,2 @@
-#!/usr/bin/env bashio
-bashio::log.info "Initializing PulseAudio..."
-pulseaudio --start
-sleep 2
-
-bashio::log.info "Launching Tetris HA..."
-exec s6-svscan /etc/services.d
+#!/usr/bin/env bash
+python3 /main.py
